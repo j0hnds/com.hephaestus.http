ef84e8e4846b6c64db0754aa7b096e0e2f84224d
j0hnds <dj0hnve@gmail.com>
2008-08-22 13:57:20 -0600

    Factored out the response controls (oops)

--- src/com/hephaestus/http/views/HTTPView.java
+++ src/com/hephaestus/http/views/HTTPView.java
@@ -4,11 +4,6 @@ import java.util.Map;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.*;
 import org.eclipse.core.runtime.Preferences.IPropertyChangeListener;
@@ -24,6 +19,7 @@ import com.hephaestus.http.Activator;
 import com.hephaestus.http.actions.InvokeURLAction;
 import com.hephaestus.http.preferences.PreferenceConstants;
 import com.hephaestus.http.views.ui.RequestData;
+import com.hephaestus.http.views.ui.ResponseData;
 import com.hephaestus.http.views.ui.URLFields;
 
 public class HTTPView extends ViewPart implements HTTPViewData,
@@ -38,14 +34,11 @@ public class HTTPView extends ViewPart implements HTTPViewData,
 	// The text field for the response status.
 	private Text tfStatus;
 
-	// The text field for the response data
-	private Text tfResultData;
-
-	// The table for the result headers
-	private Table tblResultHeaders;
-
 	// The request data control
 	private RequestData requestData;
+	
+	// The response data control
+	private ResponseData responseData;
 
 	/**
 	 * Constructs a new HTTPView object.
@@ -109,68 +102,11 @@ public class HTTPView extends ViewPart implements HTTPViewData,
 		gd = new GridData(SWT.FILL, SWT.CENTER, true, false);
 		tfStatus.setLayoutData(gd);
 
-		createResultDataFields(result);
-	}
-
-	/**
-	 * Creates the result data fields for the response.
-	 * 
-	 * @param result
-	 *            the parent control
-	 */
-	private void createResultDataFields(Composite result) {
-		TabFolder tabs = new TabFolder(result, SWT.NONE);
-		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+		responseData = new ResponseData(result, SWT.NONE);
+		gd = new GridData(SWT.FILL, SWT.FILL, true, true);
 		gd.horizontalSpan = 2;
 		gd.heightHint = 100;
-		tabs.setLayoutData(gd);
-
-		createResultHttpHeaders(tabs);
-
-		createResultData(tabs);
-	}
-
-	/**
-	 * More result fields.
-	 * 
-	 * @param tabs
-	 *            the parent for the controls.
-	 */
-	private void createResultData(TabFolder tabs) {
-		TabItem item = new TabItem(tabs, SWT.NONE);
-		item.setText("Data");
-
-		tfResultData = new Text(tabs, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL
-				| SWT.V_SCROLL);
-
-		item.setControl(tfResultData);
-	}
-
-	/**
-	 * Creates the result data for http headers.
-	 * 
-	 * @param tabs the tab folder
-	 */
-	private void createResultHttpHeaders(TabFolder tabs) {
-		TabItem item = new TabItem(tabs, SWT.NONE);
-		item.setText("HTTP Headers");
-
-		// Set up a table to enter the headers
-		tblResultHeaders = new Table(tabs, SWT.MULTI | SWT.BORDER
-				| SWT.FULL_SELECTION);
-		tblResultHeaders.setLinesVisible(true);
-		tblResultHeaders.setHeaderVisible(true);
-
-		// Set up the columns
-		TableColumn col = new TableColumn(tblResultHeaders, SWT.NONE);
-		col.setText("Header");
-		col.setWidth(100);
-
-		col = new TableColumn(tblResultHeaders, SWT.NONE);
-		col.setText("Value");
-		col.setWidth(100);
-		item.setControl(tblResultHeaders);
-
+		responseData.setLayoutData(gd);
 	}
 
 	/**
@@ -247,17 +183,11 @@ public class HTTPView extends ViewPart implements HTTPViewData,
 	}
 
 	public void setResponseData(String data) {
-		tfResultData.setText(data);
+		responseData.setResponseData(data);
 	}
 
 	public void setResponseHttpHeaders(Map<String, String> headers) {
-		tblResultHeaders.removeAll();
-
-		for (String header : headers.keySet()) {
-			TableItem ti = new TableItem(tblResultHeaders, SWT.NONE);
-			ti.setText(0, header);
-			ti.setText(1, headers.get(header));
-		}
+		responseData.setResponseHttpHeaders(headers);
 	}
 
 	public void setStatus(String status) {
